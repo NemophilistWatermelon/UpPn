4229e28c631e69a4ce5f982d48bda7779947e085
WaterMelon <937817514@qq.com>
2022-04-23 21:47:31 +0800

    feat: add file

--- my-vue-app/src/pages/UdPn/Top/UI/index.vue
+++ my-vue-app/src/pages/UdPn/Top/UI/index.vue
@@ -1,31 +1,39 @@
-<script>
-  import { defineComponent, reactive, ref} from 'vue'
-  export default defineComponent({
-    name: 'UI',
-    setup() {
-      let BindText = ref('BindText')
-
-      return {
-        BindText
-      }
-    }
+<script lang="ts" setup>
+  import {ref, defineProps, withDefaults} from 'vue'
+  const prop = defineProps({
+    normalText: String
   })
+  let bool = ref(false)
+  let BindText = ref('')
+  console.log(prop)
+
+  function onInputChange(e) {
+    if (e !== prop.normalText) {
+      bool.value = true
+      return
+    }
+    return bool.value = false
+  }
+
 </script>
 
 <template>
   <div class="ui-item">
     <section>文字</section>
-    <el-input v-model="BindText"></el-input>
+    <el-input @input="onInputChange" v-model="BindText"></el-input>
+    <span v-if="bool">输错了</span>
+    <span v-else>Yep! you win</span>
   </div>
 </template>
 
 
 <style scoped lang="scss">
 .ui-item {
-    width: 100px;
-    height: 100px;
-    text-align: center;
-    padding: 10px;
-    box-sizing: border-box;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  padding: 10px;
+  box-sizing: border-box;
+  outline: 1px solid red;
 }
 </style>
--- my-vue-app/src/pages/UdPn/Word/word.js
+++ my-vue-app/src/pages/UdPn/Word/word.js
@@ -1,7 +1,53 @@
 const wordsMap = {
+  rule: {
+    自然码: {
+      a: 'a | ',
+      b: 'b |',
+      c: 'c | iao',
+      d: 'd | uang',
+      e: 'e |',
+      f: 'f | en',
+      g: 'g | eng',
+      h: 'h | ang',
+      i: 'i | ch',
+      j: 'j | an',
+      k: 'k | ao',
+      l: 'l | ai',
+      m: 'm | ian',
+      n: 'n | in',
+      o: 'o | uo',
+      p: 'p | un',
+      q: 'q | iu',
+      r: 'r | uan',
+      s: 's | ong | iong',
+      t: 't | ue',
+      u: 'u | sh',
+      v: 'zh | v | ui',
+      w: 'w | ia | ua',
+      x: 'x | ie',
+      y: 'y | uai | ing',
+      z:  'z | ei'
+    }
+  },
   // 简单模式
   EASYMODE: {
-  } 
+    秦: 'qn',
+    孟: 'mg',
+    孙: 'sp',
+  },
+  组词: {
+    赵钱: 'vkqm',
+    熊猫: 'xsmk',
+    盒子: 'hezi',
+    海绵: 'hlmm',
+    理想: 'lixd',
+    爬虫: 'pais',
+    小猫: 'xcmk',
+  },
+  单字母练习: {
+    额: 'ee',
+    啊: 'aa'
+  }
 }
 
 export { 
