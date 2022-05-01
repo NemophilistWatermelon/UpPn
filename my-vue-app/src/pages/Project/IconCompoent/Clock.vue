e06e39ed92c6736d8332e5c574abf9977d90aeb1
Ronn <ronn97@qq.com>
2022-05-01 11:57:34 +0800

    feat: add project

---
+++ my-vue-app/src/pages/Project/IconCompoent/Clock.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M128 230a102 102 0 1 1 102-102a102.2 102.2 0 0 1-102 102Zm0-192a90 90 0 1 0 90 90a90.1 90.1 0 0 0-90-90Zm62 90a6 6 0 0 0-6-6h-50V72a6 6 0 0 0-12 0v56a6 6 0 0 0 6 6h56a6 6 0 0 0 6-6Z"></path></svg>
+</template>
+
+<script>
+export default {
+  name: 'PhClockLight'
+}
+</script>
--- my-vue-app/src/pages/Project/index.vue
+++ my-vue-app/src/pages/Project/index.vue
@@ -1,3 +1,107 @@
+<script>
+import Template from '../Blog/template.vue';
+import Clock from './IconCompoent/Clock.vue';
+import Excel from './IconCompoent/Excel.vue';
+
+export default defineComponent({
+  name: 'Project',
+
+  components: {
+    Clock,
+    Excel,
+    Template
+  },
+  setup() {
+    const mdRouter = reactive({
+      ProjectRoute: [
+        {
+          title: "即将到来",
+          children: [
+            {
+              title: "JSON TO EXCEL",
+              desc: "一个在线工具实现EXCEL导出",
+              path: "https://json2excel.vercel.app/"
+              // icon: <Excel />,
+            },
+            {
+              title: "JSON TO EXCEL",
+              desc: "一个在线工具实现EXCEL导出",
+              path: "https://json2excel.vercel.app/"
+              // icon: <Excel />,
+            }
+          ]
+        },
+        {
+          title: "最新的"
+        }
+      ]
+    })
+
+    function getNewsDetail(url) {
+      window.open(url, "_blank");
+    }
+
+    return {
+      getNewsDetail,
+      mdRouter
+    }
+  },
+})
+</script>
+
 <template>
-  project
+  <div class="project_wrap max-full">
+    <h1> 项目 </h1>
+    <p>我引以为豪的项目清单</p>
+    <div class="project_list">
+      <div class="project_item" v-for="item in mdRouter.ProjectRoute">
+        <template v-if="item?.children?.length">
+          <h2>{{ item.title }}</h2>
+          <div class="project_item_list">
+            <div class="project_item_item" v-for="item in item.children" @click="getNewsDetail(item.path)">
+              <div class="project_item_item_icon">
+                <Excel />
+              </div>
+              <div class="project_item_item_content">
+                <h3>{{ item.title }}</h3>
+                <p>{{ item.desc }}</p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.project_wrap {
+  .project_item {
+    padding-top: 15px;
+
+    .project_item_list {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+
+      .project_item_item {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        opacity: 0.5;
+        padding: 10px;
+
+        &:hover {
+          opacity: 1;
+        }
+
+        .project_item_item_icon {
+          display: flex;
+          font-size: 48px;
+        }
+      }
+    }
+  }
+
+}
+</style>
