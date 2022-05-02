88b4c282e0f51865b624f7647e6e13e750e70081
WaterMelon <937817514@qq.com>
2022-05-02 19:55:45 +0800

    feat: 新增跳转主页点击 logo

--- my-vue-app/src/layout/Top/LogoV2.vue
+++ my-vue-app/src/layout/Top/LogoV2.vue
@@ -1,11 +1,23 @@
+<script setup>
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+
+  const onClickJump = function() {
+    router.push({
+      path: '/'
+    })
+  }
+</script>
+
 <template>
-  <img src="./img/self.png" alt="">
+  <img @click="onClickJump" src="./img/self.png" alt="">
 </template>
 
 <style lang="scss" scoped>
 img {
-  width: 5em;
-  height: 3em;
+  cursor: pointer;
+  width: 8em;
+  height: 5em;
   filter: var(--logo-filter-color);
 }
 
