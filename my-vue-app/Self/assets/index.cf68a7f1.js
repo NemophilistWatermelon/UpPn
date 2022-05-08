9d1f1ebaab2fcbb1df160d40d63e8c14b5d33836
WaterMelon <937817514@qq.com>
2022-05-08 20:21:48 +0800

    fix: 样式修改

--- my-vue-app/Self/assets/index.cf68a7f1.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.e8f141f3.js";import{o as _,f as s,y as r}from"./vendor.de3d28b0.js";const a={},o={class:"defalut-text title"};function n(e,d){return _(),s("div",o,r(e.$attrs.title),1)}var f=t(a,[["render",n],["__scopeId","data-v-19dfa0f2"]]);export{f as T};
